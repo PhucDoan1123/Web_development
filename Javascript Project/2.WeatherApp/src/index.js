a6af2e7c0f29165fdcd87210b430a6bc8c9cb895
Phucdt <phucdtce170580@fpt.edu.vn>
2024-04-12 19:35:24 +0700

    complex project

---
+++ Javascript Project/2.WeatherApp/src/index.js
@@ -0,0 +1,53 @@
+const apiKey = "e1f50dc6c7d097fabf20a71bcdc46f86";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=vi&q=";
+
+const inputCity = document.querySelector(".input-bar");
+const searchBtn = document.querySelector(".search-button");
+const weatherIcon = document.querySelector(".weather-image");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  const data = await response.json();
+
+  if (response.status == 404) {
+    alert("This city do not exist!");
+  }
+  console.log(data);
+
+  document.querySelector(".city-name").innerHTML = data.name;
+  document.querySelector(".temperature").innerHTML =
+    Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML =
+    (data.wind.speed * 3.6).toFixed(2) + "km/h";
+  switch (data.weather[0].main) {
+    case "Clouds": {
+      weatherIcon.src = "images/clouds.png";
+      break;
+    }
+    case "Clear": {
+      weatherIcon.src = "images/clear.png";
+      break;
+    }
+    case "Rain": {
+      weatherIcon.src = "images/rain.png";
+      break;
+    }
+    case "Drizzle": {
+      weatherIcon.src = "images/drizzle.png";
+      break;
+    }
+    case "Mist": {
+      weatherIcon.src = "images/mist.png";
+      break;
+    }
+    default: {
+      weatherIcon.src = "images/clear.png";
+    }
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(inputCity.value);
+});
