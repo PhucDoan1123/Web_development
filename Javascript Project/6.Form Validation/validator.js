3ecd6bc63d63e4b34fe2b2e03facf7741296d626
Phucdt <phucdtce170580@fpt.edu.vn>
2024-04-24 17:03:21 +0700

    fea: get data from form input

--- Javascript Project/6.Form Validation/validator.js
+++ Javascript Project/6.Form Validation/validator.js
@@ -1,10 +1,19 @@
 function Validator(option) {
-  //Hàm thực hiện validate
+  var selectorRules = {};
+
+  // Function to perform validation
   const validate = (inputElement, rule) => {
     var errorElement = inputElement.parentElement.querySelector(
       option.errorSelector
     );
-    var errorMessage = rule.test(inputElement.value);
+    var errorMessage;
+    var rules = selectorRules[rule.selector];
+
+    for (var i = 0; i < rules.length; ++i) {
+      errorMessage = rules[i](inputElement.value);
+      if (errorMessage) break;
+    }
+
     if (errorMessage) {
       errorElement.innerText = errorMessage;
       inputElement.parentElement.classList.add("invalid");
@@ -12,22 +21,70 @@ function Validator(option) {
       errorElement.innerText = "";
       inputElement.parentElement.classList.remove("invalid");
     }
+    return !errorMessage;
   };
 
-  //Lấy element của form cần validate
+  // Get the form element to validate
   var formElement = document.querySelector(option.form);
 
   if (formElement) {
+    //Validate if submit form before full filed
+    formElement.onsubmit = function (e) {
+      e.preventDefault();
+
+      var isFormValid = true;
+
+      //Go through all of rules and do validate
+      option.rules.forEach(function (rule) {
+        var inputElement = formElement.querySelector(rule.selector);
+        var isValid = validate(inputElement, rule);
+        if (!isValid) {
+          isFormValid = false;
+        }
+      });
+
+      console.log();
+
+      if (isFormValid) {
+        //submit with javascript
+        if (typeof option.onSubmit === "function") {
+          var enableInputs = formElement.querySelectorAll(
+            "[name]:not([disabled])"
+          );
+          var formValues = Array.from(enableInputs).reduce(function (
+            values,
+            input
+          ) {
+            values[input.name] = input.value;
+            return values;
+          },
+          {});
+          option.onSubmit(formValues);
+
+          //submit with default behavior of browser
+        } else {
+          formElement.submit();
+        }
+      }
+    };
+
     option.rules.forEach(function (rule) {
+      // Save the rules for each input
+      if (selectorRules[rule.selector]) {
+        selectorRules[rule.selector].push(rule.test);
+      } else {
+        selectorRules[rule.selector] = [rule.test];
+      }
+
       var inputElement = formElement.querySelector(rule.selector);
 
       if (inputElement) {
-        //Xử lí khi trường hợp blur khỏi input
+        // Handle blur event
         inputElement.onblur = function () {
           validate(inputElement, rule);
         };
 
-        //Xử lí mỗi khi người dùng nhập input
+        // Handle input event
         inputElement.oninput = function () {
           var errorElement = inputElement.parentElement.querySelector(
             option.errorSelector
@@ -40,41 +97,47 @@ function Validator(option) {
   }
 }
 
-Validator.isRequired = function (selector) {
+// Define validation rules
+
+Validator.isRequired = function (selector, message) {
   return {
     selector: selector,
     test: function (value) {
-      return value.trim() ? undefined : "Please require this flied";
+      return value.trim() ? undefined : message || "Please require this field";
     },
   };
 };
 
-Validator.isEmail = function (selector) {
+Validator.isEmail = function (selector, message) {
   return {
     selector: selector,
     test: function (value) {
       var regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-      return regex.test(value) ? undefined : "This field must be an email";
+      return regex.test(value)
+        ? undefined
+        : message || "This field must be an email";
     },
   };
 };
 
-Validator.minLength = function (selector, min) {
+Validator.minLength = function (selector, min, message) {
   return {
     selector: selector,
     test: function (value) {
       return value.length >= min
         ? undefined
-        : `This filed must greater than ${min} characters`;
+        : message || `This field must have at least ${min} characters`;
     },
   };
 };
 
-Validator.isConfirmed = function (selector, getConfirmValue) {
+Validator.isConfirmed = function (selector, getConfirmValue, message) {
   return {
     selector: selector,
     test: function (value) {
-      return value === getConfirmValue() ? undefined : "This value is not true";
+      return value === getConfirmValue()
+        ? undefined
+        : message || "This value does not match";
     },
   };
 };
